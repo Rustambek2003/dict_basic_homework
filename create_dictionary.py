90c9e84fb0996b3f6651abaeb882b6c8f6d53c5f
Rustambek2003 <rustambekjumaqulov@gmail.com>
2022-11-20 14:17:44 +0500

    '3'

--- create_dictionary.py
+++ create_dictionary.py
@@ -1,15 +1,16 @@
-# def create_dictionary(key, value):
-#     """
-#     Convert two lists into a dictionary
-#     Args:
-#         key(list): list of keys
-#         value(list): list of values
-#     Returns:
-#         dict: dictionary with keys and values
-#     """
+def create_dictionary(key, value):
+    """
+    Convert two lists into a dictionary
+    Args:
+        key(list): list of keys
+        value(list): list of values
+    Returns:
+        dict: dictionary with keys and values
+    """
+    a = zip(key,value)
+    ans = {}
+    for i, j in a:
+        ans[i] = j
+
     
-#     return
-key = [1, 2, 3] 
-value = ["one", "two", "three"]
-for i in zip(key):
-    print(i)
+    return ans
